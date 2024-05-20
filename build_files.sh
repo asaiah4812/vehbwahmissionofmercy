fca74e7df9106de81b46b685bd9ab87a47bbe4da
Asaiah Henson <Hensonasaiah21@gmail.com>
2024-05-20 01:32:19 -0700

    initial commit

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,7 @@
 # build_files.sh
 
-python -m pip install -r requirements.txt
-python3.9 manage.py collectstatic
+python3 -m pip install -r requirements.txt
+python3 manage.py collectstatic --noinput
+
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
