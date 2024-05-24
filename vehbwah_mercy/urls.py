5394e04ae27b822de29da284f3768afc3c5e37f3
Asaiah Henson <Hensonasaiah21@gmail.com>
2024-05-23 17:30:58 -0700

    intial commit

--- vehbwah_mercy/urls.py
+++ vehbwah_mercy/urls.py
@@ -9,5 +9,4 @@ urlpatterns = [
     path('summernote/', include('django_summernote.urls')),
 ]
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
